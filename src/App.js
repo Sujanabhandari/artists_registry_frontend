20af4d6cdffced8bb8ed5fe85800d421f479323d
Sujana Bhandari <sujana6589@gmail.com>
2022-12-15 15:19:15 +0100

    Add toast notification and loading function

--- src/App.js
+++ src/App.js
@@ -2,34 +2,72 @@ import { useEffect, useState, useRef } from "react";
 import axios from 'axios';
 import ArtistInfo from "./components/ArtistInfo";
 import { debounce } from 'lodash';
+import PacmanLoader from "react-spinners/ClipLoader";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+const override = {
+  display: "block",
+  margin: "0 auto",
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  borderWidth: "3px"
+};
 
 function App() {
   const [artists, setArtists] = useState([]);
-  const input_field = useRef(null);
+  const input_field = useRef([]);
+  let [loading, setLoading] = useState(false);
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
   const handleChange = debounce(async (e) => {
-    try {
-      const { data } = await axios.get(`http://localhost:3000/api/artists?name=${e.target.value}`);
-      setArtists(data);
-    } catch (error) {
-      console.log(error);
+    if (e.target.value !== '') {
+      try {
+        setLoading(true)
+        const { data } = await axios.get(`http://localhost:3000/api/artists?name=${e.target.value}`);
+        setArtists(data);
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        console.log(error);
+      }
+    } else {
+      setArtists([]);
     }
   }, 150);
 
   return (
-    <div className="container-fluid">
-      <div className='bg-primary py-5 w-auto d-flex align-items-center justify-content-center'>
+    <>
+    <ToastContainer limit={5} />
+    <div className="container-fluid main_page">
+      <div className='py-5 w-auto d-flex align-items-center justify-content-center'  style={{backgroundColor: "#8e02ef"}}>
         <h3 className='text-white'>Welcome to Artist Registry</h3>
       </div>
-      <form className="d-flex mx-0 mx-md-auto d-noned-md-inline-flex search col col-md-6 order-1 order-md-2 mt-5" role="search">
-        <input className="form-control me-2" type="search"
-          placeholder="Search" aria-label="Search"
+      <form className="d-flex mx-0 mx-md-auto d-noned-md-inline-flex search col col-md-6 order-1 order-md-2 mt-5" role="search" 
+      onSubmit={handleSubmit}>
+        <input className="form-control p-3 border rounded-5 border-2" type="search"
+          placeholder="Search Artist" aria-label="Search"
           onChange={handleChange}
           ref={input_field}
         />
       </form>
-      <ArtistInfo artists={artists} />
+      {/* <img src='./music.png'/> */}
+      <PacmanLoader
+                color={"#ffc008"}
+                loading={loading}
+                cssOverride={override}
+                size={150}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
+      <ArtistInfo artists={artists}/>
     </div>
+    </>
   );
 }
 
