c0b840af9dfff412359990e7fd785ea387184b1c
Sujana Bhandari <sujana6589@gmail.com>
2022-12-15 16:57:17 +0100

    Refactor and Lint code

--- src/App.js
+++ src/App.js
@@ -7,68 +7,67 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 const override = {
-  display: "block",
-  margin: "0 auto",
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  borderWidth: "3px"
+    display: "block",
+    margin: "0 auto",
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    borderWidth: "3px"
 };
 
 function App() {
-  const [artists, setArtists] = useState([]);
-  const input_field = useRef([]);
-  let [loading, setLoading] = useState(false);
+    const [artists, setArtists] = useState([]);
+    const input_field = useRef([]);
+    let [loading, setLoading] = useState(false);
 
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
 
-  const handleChange = debounce(async (e) => {
-    if (e.target.value !== '') {
-      try {
-        setLoading(true)
-        const { data } = await axios.get(`http://localhost:3000/api/artists?name=${e.target.value}`);
-        setArtists(data);
-        setLoading(false)
-      } catch (error) {
-        setLoading(false)
-        console.log(error);
-      }
-    } else {
-      setArtists([]);
-    }
-  }, 150);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
 
-  return (
-    <>
-    <ToastContainer limit={5} />
-    <div className="container-fluid main_page">
-      <div className='py-5 w-auto d-flex align-items-center justify-content-center'  style={{backgroundColor: "#8e02ef"}}>
-        <h3 className='text-white'>Welcome to Artist Registry</h3>
-      </div>
-      <form className="d-flex mx-0 mx-md-auto d-noned-md-inline-flex search col col-md-6 order-1 order-md-2 mt-5" role="search" 
-      onSubmit={handleSubmit}>
-        <input className="form-control p-3 border rounded-5 border-2" type="search"
-          placeholder="Search Artist" aria-label="Search"
-          onChange={handleChange}
-          ref={input_field}
-        />
-      </form>
-      {/* <img src='./music.png'/> */}
-      <PacmanLoader
-                color={"#ffc008"}
-                loading={loading}
-                cssOverride={override}
-                size={150}
-                aria-label="Loading Spinner"
-                data-testid="loader"
-            />
-      <ArtistInfo artists={artists}/>
-    </div>
-    </>
-  );
+    const handleChange = debounce(async (e) => {
+        if (e.target.value !== '') {
+            try {
+                setLoading(true)
+                const { data } = await axios.get(`http://localhost:3000/api/artists?name=${e.target.value}`);
+                setArtists(data);
+                setLoading(false)
+            } catch (error) {
+                setLoading(false)
+                console.log(error);
+            }
+        } else {
+            setArtists([]);
+        }
+    }, 300);
+
+    return (
+        <>
+            <ToastContainer limit={5} />
+            <div className="main_page">
+                <div className='py-5 w-auto d-flex align-items-center justify-content-center container-fluid ' style={{ backgroundColor: "#8e02ef" }}>
+                    <h3 className='text-white'>Welcome to Artist Registry</h3>
+                </div>
+                <form className="d-flex mx-0 mx-md-auto d-noned-md-inline-flex search col col-md-6 order-1 order-md-2 mt-5" role="search"
+                    onSubmit={handleSubmit}>
+                    <input className="form-control p-3 border rounded-5 border-2" type="search"
+                        placeholder="Search Artist" aria-label="Search"
+                        onChange={handleChange}
+                        ref={input_field}
+                    />
+                </form>
+                <PacmanLoader
+                    color={"#ffc008"}
+                    loading={loading}
+                    cssOverride={override}
+                    size={150}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+                <ArtistInfo artists={artists} />
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/components/ArtistInfo.js
+++ src/components/ArtistInfo.js
@@ -3,69 +3,68 @@ import { toast } from "react-toastify";
 
 const ArtistInfo = ({ artists }) => {
 
-    const [filename, setFilename] =useState("artist-data.csv");
+    const [filename, setFilename] = useState("artist-data.csv");
     const handleDownloadClick = () => {
-      let csv = "name,mbid,url,image_small,image\n";
-      artists.forEach(artist => {
-        csv += `${artist.name},${artist.mbid},${artist.url},${artist.image_small},${artist.image}\n`;
-      });
-  
-      const blob = new Blob([csv], { type: "text/csv" });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.setAttribute("download", filename);
-      a.setAttribute("href", url);
-      a.click();
-      toast.success("Congratulations!CSV File  is successfully exported")
+        let csv = "name,mbid,url,image_small\n";
+        artists.forEach(artist => {
+            const image_small = artist?.image?.find(image => image?.size === 'small')['#text']
+            csv += `${artist.name},${artist.mbid},${artist.url},${image_small}\n`;
+        });
+
+        const blob = new Blob([csv], { type: "text/csv" });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.setAttribute("download", filename);
+        a.setAttribute("href", url);
+        a.click();
+        toast.success("Congratulations! CSV File  is successfully exported.")
     };
-  
+
     const handleFilenameChange = (event) => {
-      setFilename(event.target.value);
+        setFilename(event.target.value);
     };
-  
+
     return (
-      <>
-        <section className="mt-5">
-          <div className="container px-0">
-            {artists.length > 0 && (
-              <div className="m-4 text-center">
-                <label htmlFor="filename" className="h5">Enter a file name:</label>
-                <input
-                  id="filename"
-                  value={filename}
-                  className="ms-3 border p-2 border-opacity-50 rounded"
-                  onChange={handleFilenameChange}
-                />
-                <button onClick={handleDownloadClick} className="btn btn-warning">
-                  Download artist data
-                </button>
+        <>
+            <div className="container px-0">
+                {artists.length > 0 && (
+                    <div className="m-4 text-center">
+                        <label htmlFor="filename" className="h5">Enter a file name:</label>
+                        <input
+                            id="filename"
+                            value={filename}
+                            className="ms-3 border p-2 border-opacity-50 rounded"
+                            onChange={handleFilenameChange}
+                        />
+                        <button onClick={handleDownloadClick} className="btn btn-warning ms-1">
+                            Export
+                        </button>
+                    </div>
+                )}
+
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-5">
+                    {artists?.map((artist, index) => (
+                        <div className="card m-2" style={{ width: "18rem" }} key={index}>
+                            <div className="card-body">
+                                <h5 className="card-title">{artist?.name}</h5>
+                                <p className="card-text">Listeners{artist?.listeners}</p>
+                                <p className="card-text">Mbid {artist?.mbid}</p>
+                                <img src={artist?.image?.find(image => image?.size === 'small')['#text'] || "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png"} alt={artist?.name} />
+                                <a
+                                    href={artist?.url}
+                                    className="btn btn-secondary ms-5"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    URL
+                                </a>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            )}
-  
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-5">
-              {artists?.map((artist, index) => (
-                <div className="card m-2" style={{ width: "18rem" }} key={index}>
-                  <div className="card-body">
-                    <h5 className="card-title">{artist?.name}</h5>
-                    <p className="card-text">Listeners{artist?.listeners}</p>
-                    <p className="card-text">Mbid {artist?.mbid}</p>
-                    <img src={artist?.image?.find(image => image?.size === 'small')['#text']} alt={artist?.name} />
-                    <a
-                      href={artist?.url}
-                      className="btn btn-secondary ms-5"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      URL
-                    </a>
-                  </div>
-                </div>
-              ))}
             </div>
-          </div>
-        </section>
-      </>
+        </>
     );
-  };
-  
-  export default ArtistInfo;
+};
+
+export default ArtistInfo;
