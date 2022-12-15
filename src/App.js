d585fe07e7af12d83ea97c4eecc90f1f550238d4
Sujana Bhandari <sujana6589@gmail.com>
2022-12-15 11:54:36 +0100

    Search artist by name and display in card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import axios from 'axios';
+import ArtistInfo from "./components/ArtistInfo";
+import { debounce } from 'lodash';
 
 function App() {
+  const [artists, setArtists] = useState([]);
+  const input_field = useRef(null);
+
+  const handleChange = debounce(async (e) => {
+    try {
+      const { data } = await axios.get(`http://localhost:3000/api/artists?name=${e.target.value}`);
+      setArtists(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 150);
+
   return (
-    <div className="App">
-     <h2>Artist Registry</h2>
+    <div className="container-fluid">
+      <div className='bg-primary py-5 w-auto d-flex align-items-center justify-content-center'>
+        <h3 className='text-white'>Welcome to Artist Registry</h3>
+      </div>
+      <form className="d-flex mx-0 mx-md-auto d-noned-md-inline-flex search col col-md-6 order-1 order-md-2 mt-5" role="search">
+        <input className="form-control me-2" type="search"
+          placeholder="Search" aria-label="Search"
+          onChange={handleChange}
+          ref={input_field}
+        />
+      </form>
+      <ArtistInfo artists={artists} />
     </div>
   );
 }
